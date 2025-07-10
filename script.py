1823c9a0799284aaae95d58e97adb398df088e20
Hemit Patel <hemitvpatel@gmail.com>
2025-07-10 16:14:01 +0000

    added pdf reporting tool

--- script.py
+++ script.py
@@ -1,4 +1,25 @@
 import requests
+from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.units import inch
+
+# Report document to build PDF file
+document = []
+document.append(Image('logo.jpeg', 1.1*inch,1.1*inch))
+
+title_style = ParagraphStyle(
+    name='Title',
+    fontFamily='Helvetica',
+    fontSize=36
+)
+
+paragraph_style = ParagraphStyle(
+    name='Paragraph',
+    fontFamily='Helvetica',
+    fontSize=15
+)
 
 def fetch_substations(north, east, south, west):
     url = "https://overpass-api.de/api/interpreter"
@@ -21,6 +42,25 @@ def fetch_substations(north, east, south, west):
     else:
         raise Exception(f"Error {response.status_code}: {response.text}")
 
+def add_title(title):
+    document.append(Spacer(1,20))
+    document.append(Paragraph(title, title_style))
+    document.append(Spacer(1,50))
+
+def add_paragraph(paragraph):
+    document.append(Spacer(1,15))
+    document.append(Paragraph(paragraph, paragraph_style))
+    document.append(Spacer(1,30))
+
+
+def get_aeso_api():
+    # For those in Alberta
+    
+
+def get_ercot_api():
+    # For those in Texas
+
+
 def run_program():
     north = input("Enter north-bound coordinate:")
     east = input("Enter east-bound coordinate:")
@@ -29,20 +69,22 @@ def run_program():
     provider = input("Is the provider AESO or ERCOT:").upper()
 
     if (provider not in ["AESO", "ERCOT"]):
-        print("Provider must be AESO or ERCOT")
+        print("❌ Provider must be AESO or ERCOT")
         return
 
     substations = fetch_substations(north, east, south, west)
 
     for item in substations:
-        tags = item.get("tags", {})
-        voltage = tags.get("voltage")
+        tags = item.get("tags")
+        name = tags.get("name")
 
-        if voltage:
-            print(voltage)
-            print(item)
+        if name:
+            add_title(name)
+            add_paragraph('''
+            
+            ''')
 
-
+    SimpleDocTemplate('report.pdf', pagesize=letter, rightMargin=12, leftMargin=12, topMargin=12, bottomMargin=6).build(document)
 
 if __name__ == "__main__":
     run_program()
